1ee6e6cb21e22610dca6cca7236951f283fd6f9e
Evan Preslar <evanp5195@gmail.com>
2016-12-12 18:52:21 -0600

    renamed project and plugin, removed redundant files, updated readme

---
+++ Plugins/SceneCaptureCubeHDRRender/Source/SceneCaptureCubeHDRRender/Private/SceneCaptureCubeHDRRenderBPLibrary.cpp
@@ -0,0 +1,55 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "SceneCaptureCubeHDRRenderPrivatePCH.h"
+#include "SceneCaptureCubeHDRRenderBPLibrary.h"
+
+#define LOCTEXT_NAMESPACE "USceneCaptureCubeHDRRenderBPLibrary"
+
+USceneCaptureCubeHDRRenderBPLibrary::USceneCaptureCubeHDRRenderBPLibrary(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+
+}
+
+void USceneCaptureCubeHDRRenderBPLibrary::ExportRenderTargetCube(UObject* WorldContextObject, UTextureRenderTargetCube* TextureRenderTarget, const FString& FilePath, const FString& FileName)
+{
+	FString TotalFileName = FPaths::Combine(*FilePath, *FileName);
+	FText PathError;
+	FPaths::ValidatePath(TotalFileName, &PathError);
+
+	if (TextureRenderTarget && !FileName.IsEmpty() && PathError.IsEmpty())
+	{
+		FArchive* Ar = IFileManager::Get().CreateFileWriter(*TotalFileName);
+
+		if (Ar)
+		{
+			FBufferArchive Buffer;
+			bool bSuccess = FImageUtils::ExportRenderTargetCubeAsHDR(TextureRenderTarget, Buffer);
+
+			if (bSuccess)
+			{
+				Ar->Serialize(const_cast<uint8*>(Buffer.GetData()), Buffer.Num());
+			}
+
+			delete Ar;
+		}
+		else
+		{
+			FMessageLog("Blueprint").Warning(LOCTEXT("ExportRenderTarget_FileWriterFailedToCreate", "ExportRenderTarget: FileWrite failed to create."));
+		}
+	}
+
+	else if (!TextureRenderTarget)
+	{
+		FMessageLog("Blueprint").Warning(LOCTEXT("ExportRenderTarget_InvalidTextureRenderTarget", "ExportRenderTarget: TextureRenderTarget must be non-null."));
+	}
+	if (!PathError.IsEmpty())
+	{
+		FMessageLog("Blueprint").Warning(FText::Format(LOCTEXT("ExportRenderTarget_InvalidFilePath", "ExportRenderTarget: Invalid file path provided: '{0}'"), PathError));
+	}
+	if (FileName.IsEmpty())
+	{
+		FMessageLog("Blueprint").Warning(LOCTEXT("ExportRenderTarget_InvalidFileName", "ExportRenderTarget: FileName must be non-empty."));
+	}
+}
+
---
+++ Source/SamplePluginExport/SamplePluginExportGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SamplePluginExportGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAMPLEPLUGINEXPORT_API ASamplePluginExportGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
