e2305a9888147e095d07f5ccb5403f3ea69439a3
Evan Preslar <evanp5195@gmail.com>
2017-03-12 17:05:11 -0500

    updated Readme, added more input checks with error output, adjusted output filename format, added isActive toggle to blueprint, enabled Fixed framerate on project, replaced blueprint with one inherited from SceneCapture2D

--- Plugins/SceneCaptureCubeHDRRender/Source/SceneCaptureCubeHDRRender/Private/SceneCaptureCubeHDRRenderBPLibrary.cpp
+++ Plugins/SceneCaptureCubeHDRRender/Source/SceneCaptureCubeHDRRender/Private/SceneCaptureCubeHDRRenderBPLibrary.cpp
@@ -70,10 +70,11 @@ void USceneCaptureCubeHDRRenderBPLibrary::CaptureAndExport2DCaptureSet(
 	FText PathError;
 	FPaths::ValidatePath(FilePathToValidate, &PathError);
 
-	if (NumLongitudinalSections > 0 || NumLatitudinalSections > 0 && RenderTargetXResolution > 0 && RenderTargetYResolution > 0 && !BaseFileName.IsEmpty() && PathError.IsEmpty())
+	if (SceneCapture && EyeDistance >= 0.0 && NumLongitudinalSections > 0 && NumLatitudinalSections > 0 && RenderTargetXResolution > 0 && RenderTargetYResolution > 0 && !BaseFileName.IsEmpty() && PathError.IsEmpty())
 	{
 		float HorizontalStep = 360.0 / (NumLongitudinalSections);
 		float VerticalStep = 180.0 / (NumLatitudinalSections - 1); /* one less division because the first and last images will not be the same view, (Rotaes about Y axis from 0 to 180) */
+		bool isStereo = EyeDistance == 0.0 ? false : true;
 		float EyeDistanceFromCenter = EyeDistance / 2.0;
 		SceneCapture->FOVAngle = FOVAngle;
 
@@ -87,22 +88,27 @@ void USceneCaptureCubeHDRRenderBPLibrary::CaptureAndExport2DCaptureSet(
 
 			for (int j = 0; j < NumLatitudinalSections; j++)
 			{
-				UTextureRenderTarget2D* LeftEyeRenderTarget = UKismetRenderingLibrary::CreateRenderTarget2D(WorldContextObject, RenderTargetXResolution, RenderTargetYResolution);
 				UTextureRenderTarget2D* RightEyeRenderTarget = UKismetRenderingLibrary::CreateRenderTarget2D(WorldContextObject, RenderTargetXResolution, RenderTargetYResolution);
-
+				
 				/* Render right eye */
-				SceneCapture->TextureTarget = LeftEyeRenderTarget;
-				FString CompleteFileName = BaseFileName + FString::Printf(TEXT("_%d_X%05d_Y%05d_R.hdr"), FrameNum, i, j);
+				SceneCapture->TextureTarget = RightEyeRenderTarget;
+
+				FString CompleteFileName = BaseFileName + FString::Printf(TEXT("_%04d_X%04d_Y%04d_R.hdr"), FrameNum, i, j);
 				SceneCapture->AddLocalOffset(*(new FVector(0.0, EyeDistanceFromCenter, 0.0)));
 				SceneCapture->CaptureScene();
-				UKismetRenderingLibrary::ExportRenderTarget(WorldContextObject, LeftEyeRenderTarget, FilePath, CompleteFileName);
-
-				/* Render left eye */
-				SceneCapture->TextureTarget = RightEyeRenderTarget;
-				CompleteFileName = BaseFileName + FString::Printf(TEXT("_%d_X%05d_Y%05d_L.hdr"), FrameNum, i, j);
-				SceneCapture->AddLocalOffset(*(new FVector(0.0, -EyeDistance, 0.0)));
-				SceneCapture->CaptureScene();
 				UKismetRenderingLibrary::ExportRenderTarget(WorldContextObject, RightEyeRenderTarget, FilePath, CompleteFileName);
+
+				if (isStereo)
+				{
+					UTextureRenderTarget2D* LeftEyeRenderTarget = UKismetRenderingLibrary::CreateRenderTarget2D(WorldContextObject, RenderTargetXResolution, RenderTargetYResolution);
+
+					/* Render left eye */
+					SceneCapture->TextureTarget = LeftEyeRenderTarget;
+					CompleteFileName = BaseFileName + FString::Printf(TEXT("_%04d_X%04d_Y%04d_L.hdr"), FrameNum, i, j);
+					SceneCapture->AddLocalOffset(*(new FVector(0.0, -EyeDistance, 0.0)));
+					SceneCapture->CaptureScene();
+					UKismetRenderingLibrary::ExportRenderTarget(WorldContextObject, LeftEyeRenderTarget, FilePath, CompleteFileName);
+				}
 
 				/* Return Scene Capture to root location */
 				SceneCapture->SetWorldLocation(RootLocation);
@@ -113,30 +119,42 @@ void USceneCaptureCubeHDRRenderBPLibrary::CaptureAndExport2DCaptureSet(
 		}
 	}
 
-	else if (!PathError.IsEmpty())
+	else
 	{
-		FMessageLog("Blueprint").Warning(FText::Format(LOCTEXT("ProcessAndExport2DSet_InvalidFilePath", "ProcessAndExport2DSet: Invalid file path provided: '{0}'"), PathError));
+		if (!SceneCapture)
+		{
+			FMessageLog("Blueprint").Warning(LOCTEXT("CaptureAndExport2DCaptureSet_InvalidSceneCapture", "CaptureAndExport2DCaptureSet: SceneCapture must be non-null"));
+		}
+		if (EyeDistance < 0.0)
+		{
+			FMessageLog("Blueprint").Warning(LOCTEXT("CaptureAndExport2DCaptureSet_InvalidEyeDistance", "CaptureAndExport2DCaptureSet: EyeDistance must be non-negative"));
+		}
+		if (!PathError.IsEmpty())
+		{
+			FMessageLog("Blueprint").Warning(FText::Format(LOCTEXT("CaptureAndExport2DCaptureSet_InvalidFilePath", "CaptureAndExport2DCaptureSet: Invalid file path provided: '{0}'"), PathError));
+		}
+		if (BaseFileName.IsEmpty())
+		{
+			FMessageLog("Blueprint").Warning(LOCTEXT("CaptureAndExport2DCaptureSet_InvalidFileName", "CaptureAndExport2DCaptureSet: FileName must be non-empty."));
+		}
+		if (NumLatitudinalSections <= 0)
+		{
+			FMessageLog("Blueprint").Warning(LOCTEXT("CaptureAndExport2DCaptureSet_InvalidLatitudinalSections", "CaptureAndExport2DCaptureSet: NumLatitudinalSections must be greater than 0."));
+		}
+		if (NumLongitudinalSections <= 0)
+		{
+			FMessageLog("Blueprint").Warning(LOCTEXT("CaptureAndExport2DCaptureSet_InvalidLatitudinalSections", "CaptureAndExport2DCaptureSet: NumLongitudinalSections must be greater than 0."));
+		}
+		if (RenderTargetXResolution <= 0)
+		{
+			FMessageLog("Blueprint").Warning(LOCTEXT("CaptureAndExport2DCaptureSet_InvalidXResolution", "CaptureAndExport2DCaptureSet: RenderTargetXResolution must be greater than 0."));
+		}
+		if (RenderTargetYResolution <= 0)
+		{
+			FMessageLog("Blueprint").Warning(LOCTEXT("CaptureAndExport2DCaptureSet_InvalidYResolution", "CaptureAndExport2DCaptureSet: RenderTargetYResolution must be greater than 0."));
+		}
 	}
-	if (BaseFileName.IsEmpty())
-	{
-		FMessageLog("Blueprint").Warning(LOCTEXT("ProcessAndExport2DSet_InvalidFileName", "ProcessAndExport2DSet: FileName must be non-empty."));
-	}
-	if (NumLatitudinalSections <= 0)
-	{
-		FMessageLog("Blueprint").Warning(LOCTEXT("ProcessAndExport2DSet_InvalidLatitudinalSections", "ProcessAndExport2DSet: NumLatitudinalSections must be greater than 0."));
-	}
-	if (NumLongitudinalSections <= 0)
-	{
-		FMessageLog("Blueprint").Warning(LOCTEXT("ProcessAndExport2DSet_InvalidLatitudinalSections", "ProcessAndExport2DSet: NumLongitudinalSections must be greater than 0."));
-	}
-	if (RenderTargetXResolution <= 0)
-	{
-		FMessageLog("Blueprint").Warning(LOCTEXT("ProcessAndExport2DSet_InvalidXResolution", "ProcessAndExport2DSet: RenderTargetXResolution must be greater than 0."));
-	}
-	if (RenderTargetYResolution <= 0)
-	{
-		FMessageLog("Blueprint").Warning(LOCTEXT("ProcessAndExport2DSet_InvalidYResolution", "ProcessAndExport2DSet: RenderTargetYResolution must be greater than 0."));
-	}
+		
 }
 
 
--- README.md
+++ README.md
@@ -1,10 +1,32 @@
 UnrealSceneCaptureExportPlugin
 ==============================
 
-This project was created to extend the scene capture functionality added in the Unreal Engine v4.13.
+This project was extends the Scene Capture functionality present within Unreal Engine v4.14.
 
-Specifically, it defines a plugin which exposes a Blueprint callable node that allows for the exporting of scene capture cube renders into .HDR image files. To function, it must be provided with a valid TextureRenderTargetCube reference along with a file path and file name. A sample level is provided in this repository to demonstrate the application of the plugin.
+The Project includes a C++ Plugin exposing two Blueprint callable functions, as well as a sample .uproject demonstrating both.
 
-For the plugin to function, it must be located in the Plugins/ folder inside of a Unreal Project or added directly to the Engine's Plugins/ folder. Once the plugin has been installed, the static 'Export Render Target Cube' function should become available within Blueprint. It is recommended that the function be executed sparingly, as Scene Capture Cubes are expensive to render, even at low resolutions. For this reason, the sample Blueprint executes the function only once on level start, though it is possible to execute it every tick. The quality of the resultant .HDR image is tied to the Render Target Cube object rather than the Scene Capture object itself, and in the sample Blueprint, is set to produce a 4000x2000 image.
+The .uproject itself contains some necessary blueprints and settings which are required to make full use of the plugin, meaning simply dropping the central blueprint into another project will not necessarily work. The most important changes are as follows:
 
-The source of the plugin resembles the implementation of the UKismetRenderingLibrary::ExportTexture2D function already present in the engine, although adapted to support the exporting of Scene Capture Cubes.
+* Gamemode (Sample provided): Default Pawn and PlayerController set to actors without visible meshes (unless desired)
+* Engine: General Settings: Project is set to use Fixed Frame Rate (necessary for any level sequence / matinee automation)
+* Blueprint (Sample Provided): Helpful when rendering multiple frames in succession by providing plugin function with some information regarding frame number.
+
+To utilize the Plugin, it must first be built from provided source, after which it will appear under the plugin listing within the editor (after a restart) provided it is located within the Plugins/ directory within the project.
+
+SceneCaptureCube Export Pipeline
+================================
+
+The first of these functions (ExportRenderTargetCube) provides a means of exporting SceneCaptureCubes similar to the method already present for SceneCapture2D within UKismetRenderingLibrary::ExportTexture2D.
+The CubeRenderTargets are natively output as .hdr images in an equirectangular projection.
+
+SceneCapture2D Modular 360 Degree Stereoscopic Exporter
+=======================================================
+
+The second function (CaptureAndExport2DCaptureSet) is based around manipulating a single SceneCapture2DComponent in order to export a specific number of images around a central point. To do this, many RenderTarget2D objects are created on the fly as the CaptureComponent is rotated. As expected, this consumes a large amount of RAM, which is only compounded by the amount of captures requested per frame as well as the resolutionof the created render targets.
+
+The behaviour of the function is highly configurable through parameters, of which the most critical are the number of vertical and horizontal images to be captured. For instance, if 4 longitudinal and 3 latitudinal segments are specified, the function will export 12 images, stopping to capture at each of the 4 cardinal directions, taking 3 images (down, straight ahead, and up) at each. If more segments are requested, the rotations become smaller, and the images will overlap more. To avoid capturing redundant information when a large number of segments are requested, the FOVAngle and Resolution parameters can be adjusted, allowing images to be more tightly focused, just as with any normal SceneCapture2D. Finally, the function will ouptut two stereoscopic images, one for each eye, unless the EyeDistance parameter is set to 0.0. Debug output is sent to the editor's console detailing if any parameters are invalid.
+
+In addition to this base functionality provided by the plugin, this project also includes a blueprint which assists with rendering multiple frames in succession. This blueprint allows for capturing a specific number of frames (after which the simulation will exit), skipping a specific number of frames (only rendering the frames after this point), and scheduling intermittent garbage collection. Because this blueprint inherits directly from SceneCapture2D, it can be used in a similar fashion; most importantly, it can be entirely automated via a level sequence. Provided that the project being rendered is set to utilize a fixed frame rate (otherwise all movement will occur independent of framerate), this blueprint can be used to render a sequence of images which can be ultimately used to create 360 degree stereoscopic video.
+
+It should also be noted that in more complex projects, the first frame may be captured before all resources are initialized (textures, etc), and will thus produce undesireable output. For this reason, the sample capture blueprint defaults to skip the very first frame.
+
